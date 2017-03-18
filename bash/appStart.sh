f4df4308e1fefadb46d3cc86d183801c6fc8154e
lafoed <nadzhafox@ya.ru>
2017-03-19 02:44:49 +0300

    add bash scripts

---
+++ bash/appStart.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/expect -f
+
+set USER "PavelN"
+set PATH "/var/spotworld"
+set GIT "https://github.com/Lafoed/spotworld.git"
+set PASS "epidemicpassword"
+set PASSGIT "*"
+
+set HOST1 "185.98.87.26"
+
+
+spawn ssh $USER@$HOST1;
+expect "assword:";
+send "$PASS\r";
+expect "#*";
+send "cd $PATH\n";
+expect "#*";
+send "git pull\n";
+expect "#*";
+send "NODE_ENV=production supervisor start app.js\n";
+expect "#*";
+interact;
+
+
+
+
+
+
+
+
--- bash/dbStart.sh
+++ bash/dbStart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 
 set USER "PavelN"
-set PATH "/var/spotworld"
+set PATH "/var"
 set GIT "https://github.com/Lafoed/spotworld.git"
 set PASS "epidemicpassword"
 set PASSGIT "*"
@@ -12,14 +12,13 @@ set HOST1 "185.98.87.26"
 spawn ssh $USER@$HOST1;
 expect "assword:";
 send "$PASS\r";
-expect "$*";
+expect "#*";
 send "cd $PATH\n";
-send "git pull $GIT\r";
-expect "$*";
-send "NODE_ENV supervisor start app.js";
-expect "$*";
-send "exit\r";
-expect "$*";
+expect "#*";
+send "sudo mongod --dbpath spotdb\n";
+expect "#*";
+interact;
+
 
 
 
