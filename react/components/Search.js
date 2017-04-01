ae739b2b80e008807831a3db50b5c3defd176ffb
lafoed <nadzhafox@ya.ru>
2017-04-01 12:19:23 +0300

    add material ui

--- react/components/Search.js
+++ react/components/Search.js
@@ -10,23 +10,24 @@ export default class Search extends React.Component {
         }
     }
     chipAdd(chip){
-        var {value} = this.state
+        var {value} = this.state;
         value.push(chip);
         this.setState({value:value});
     }
     chipDelete(chip, index){
-        var {value} = this.state
+        var {value} = this.state;
         value.slice(index,1);
         this.setState({value:value});
     }
 
     render() {
-        return <ChipInput
-            value={this.state.value}
-            dataSource={this.state.tags}
-            onRequestAdd={this.chipAdd.bind(this)}
-            onRequestDelete={this.chipDelete.bind(this)}
-        />;
+        return (
+            <ChipInput
+                value={this.state.value}
+                dataSource={this.state.tags}
+                onRequestAdd={this.chipAdd.bind(this)}
+                onRequestDelete={this.chipDelete.bind(this)}
+            />);
     }
 }
 
