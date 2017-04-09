069f60d71b723cf3988279296f2dc4e88168c2bf
lafoed <nadzhafox@ya.ru>
2017-04-09 16:52:43 +0300

    demo version

--- react/components/TimeFilter.js
+++ react/components/TimeFilter.js
@@ -4,6 +4,7 @@ import DatePicker from 'material-ui/DatePicker';
 import {GridList, GridTile} from 'material-ui/GridList';
 
 
+
 export default class TimeFilter extends React.Component {
     style={
         block:{
@@ -14,30 +15,54 @@ export default class TimeFilter extends React.Component {
             padding:"0"
         },
         slider:{
-            padding: "10px 20px"
+            padding: "10px 30px 12px"
         }
     }
+    state={
+        startDate:moment(),
+        endDate:moment().add(1,'d')
+    }
+
     componentDidMount(){
+        var start1day =moment().set({'hour':0,'minutes':0,'seconds':0});
+        var end2day =moment().add(1,'d').set({'hour':23,'minutes':59,'seconds':59});
+
         noUiSlider.create(this.refs.slider, {
-            start: [ 20, 40 ],
+            start: [ moment().valueOf(), moment().valueOf() + 50000000 ],
             behaviour: 'drag',
             connect: true,
             range: {
-                'min':  20,
-                'max':  80
-            }
+                'min':  start1day.valueOf(),
+                'max':  end2day.valueOf()
+            },
+            format: {
+                to:val=>moment(Math.ceil(val)).format("HH:mm"),
+                from: val=>val
+            },
+            tooltips:[true,true]
         });
+        // this.refs.slider.noUiSlider.on('update', console.log);
     }
+
+
 
     render(){
         return(
             <Paper style={this.style.block}>
                 <GridList cols={2} cellHeight="auto" padding={0}>
                     <GridTile>
-                        <Datapicker hintText="start"/>
+                        <Datapicker
+                            hintText="start"
+                            value={this.state.startDate.toDate()}
+                            maxDate={this.state.endDate.clone().add(-1,'day').toDate()}
+                            onChange={(evt,date)=>this.setState({startDate:moment(date)})}/>
                     </GridTile>
                     <GridTile>
-                        <Datapicker hintText="end"/>
+                        <Datapicker
+                            hintText="end"
+                            value={this.state.endDate.toDate()}
+                            minDate={this.state.startDate.clone().add(1,'day').toDate()}
+                            onChange={(evt,date)=>this.setState({endDate:moment(date)})}/>
                     </GridTile>
                     <GridTile cols={2} style={this.style.slider}>
                         <div ref="slider"/>
@@ -51,17 +76,24 @@ export default class TimeFilter extends React.Component {
 class Datapicker extends React.Component{
     style={
         textDate:{
+            width:"100px"
+        },
+        date:{
+            fontSize: "10px",
+            textAlign:"center"
         }
     }
     render(){
         return (
             <DatePicker
                 {...this.props}
+                style={this.style.date}
                 autoOk={true}
                 disableYearSelection={true}
                 hideCalendarDate={true}
                 textFieldStyle={this.style.textDate}
                 container="inline"
+                formatDate={date=>moment(date).format('DD MMMM YYYY')}
             />
         )
     }
